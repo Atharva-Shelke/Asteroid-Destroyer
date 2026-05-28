1f93d9525cc1443b44077c65bedfc3171a1bfe97
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-05-28 22:13:20 +0530

    Added Launcher to run the javafx application in ide

---
+++ src/main/java/com/asteroids/Launcher.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asteroids;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author admin
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(AsteroidsApplication.class);
+    }
+    
+}
