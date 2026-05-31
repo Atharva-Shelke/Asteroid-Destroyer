d69956bcc22db1645d949fd1286d740387aae543
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-05-31 18:48:19 +0530

    Removed auto generated comments and formatted the file

--- src/main/java/com/asteroids/Launcher.java
+++ src/main/java/com/asteroids/Launcher.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.asteroids;
 
 import javafx.application.Application;
 
-/**
- *
- * @author admin
- */
 public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(AsteroidsApplication.class);
-    }
-    
+	public static void main(String[] args) {
+		Application.launch(AsteroidsApplication.class);
+	}
+
 }
