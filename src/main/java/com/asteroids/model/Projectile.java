df8917e4c47ed1cd0a24a2bdc70abb2c22732b31
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-06-02 22:49:37 +0530

    Modified Projectile to remove ide generated comments and give projectile a colour

--- src/main/java/com/asteroids/model/Projectile.java
+++ src/main/java/com/asteroids/model/Projectile.java
@@ -1,20 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.asteroids.model;
 
-/**
- *
- * @author admin
- */
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
 public class Projectile extends Character {
 
-    public Projectile(int x, int y) {
-        super(new Polygon(2, -2, 2, 2, -2, 2, -2, -2), x, y);
-    }
+	public Projectile(int x, int y) {
+		super(new Polygon(2, -2, 2, 2, -2, 2, -2, -2), x, y);
+		getShape().setFill(Color.RED);
+	}
 
 }
