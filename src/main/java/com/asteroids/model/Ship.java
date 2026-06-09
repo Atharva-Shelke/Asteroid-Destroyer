1f3d3dc57135cb967a88a5d09c16b560fece195a
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-06-09 13:40:50 +0530

    Modified ship to have initial acceleration as it is in space

--- src/main/java/com/asteroids/model/Ship.java
+++ src/main/java/com/asteroids/model/Ship.java
@@ -1,5 +1,6 @@
 package com.asteroids.model;
 
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
@@ -8,6 +9,9 @@ public class Ship extends Character {
 	public Ship(int x, int y) {
 		super(new Polygon(-5, -5, 10, 0, -5, 5), x, y);
 		getShape().setFill(Color.LIMEGREEN);
+
+		super.setMovement(new Point2D(0.5, 0.0));
+		super.move();
 	}
 
 }
